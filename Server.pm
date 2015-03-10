e8ecbc7a343d08e5b83ae37b59959c37f4d0e9b6
no-glue <nikolapav1985@gmail.com>
2015-03-10 21:31:52 +0100

    server desc

--- Server.pm
+++ Server.pm
@@ -93,3 +93,63 @@ sub mask {
 
 1;
 __END__
+
+=head1 NAME
+
+Server - Perl websockets server supporting RFC 6455 standard. 
+
+=head1 SYNOPSIS
+
+Simple broadcast server for C<utf8> messages.
+
+    #!/usr/bin/env perl
+    use IO::Socket::INET;
+    use threads("yield", 
+    "stack_size" => 64 * 4096, 
+    "exit" => "threads_only", 
+    "stringify");
+    use Thread::Queue;
+    use Server;
+    use BroadcastConsumer;
+
+    my $q = Thread::Queue->new();
+    # q
+    my $clients = Thread::Queue->new();
+    # clients
+    my $broadcastThread;
+    # broadcastThread;
+    $socket = new IO::Socket::INET (
+      LocalHost => '127.0.0.1',
+      LocalPort => '8080',
+      Proto => 'tcp',
+      Listen => 10,
+      Reuse => 1
+    ) or die "Oops: $!\n";
+    print STDERR "Server is up and running\n";
+    while(1) {
+      $clientSocket = $socket->accept();
+      push @clients, $clientSocket;
+      if(defined $broadcastThread) {
+        $broadcastThread->kill("SIGTERM");
+      }
+      $thread = threads->create(sub {
+        $server = new Server();
+        $server->doHandshake($clientSocket);
+        while(1) {
+          $server->listen($clientSocket, $q);
+        }
+      });
+      $thread->detach();
+      $broadcastThread = threads->create(sub {
+        $SIG{"TERM"} = sub {threads->exit();};
+        $broadcastConsumer = new BroadcastConsumer();
+        while(1) {
+          $broadcastConsumer->broadcast(\@clients, $q);
+        }
+      });
+      $broadcastThread->detach();
+    }
+
+=head1 DESCRIPTION
+
+The module implements websockets 6455 standard. It supports concurrent connections using threads and communication based on message queue.
